9adb1da98a332d988b5060263bda77eafd8277c9
Daily Account Dev <development@dailyaccount.local>
2026-06-10 22:30:52 +0530

    Update base path to match repo name Bhukh-Lagi-Hai-

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/fridge-chef">
+    <BrowserRouter basename="/Bhukh-Lagi-Hai-">
       <App />
     </BrowserRouter>
   </React.StrictMode>
